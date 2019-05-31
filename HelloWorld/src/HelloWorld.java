7eafb3bdc5719c32585948f8d69b9ad6297ab4e7
vshadmin <vshadmin@Infva06687>
2019-05-31 12:25:12 -0700

    Day 1 Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Hello Worlddddddddddd....!!!!");
+		int a=sc.nextInt();
+		System.out.println("the number that you just entered is :"+a);
+	}
+	
+}
