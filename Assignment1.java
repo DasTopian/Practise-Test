a8482abc719408bd602ea312ce0ff0d02ab82665
DasTopian <42478654+DasTopian@users.noreply.github.com>
2019-06-01 05:18:01 -0700

    Assignment1 java
    last question for the day

---
+++ Assignment1.java
@@ -0,0 +1,12 @@
+package com.lti.training.day2.oop.basics;
+import java.lang.Integer;
+
+public class Assignment1 {
+	public static void main(String args[]) {
+		int x = 65;
+		System.out.println(Integer.toHexString(x));
+		System.out.println(Integer.toBinaryString(x));
+		System.out.println(Integer.toOctalString(x));
+	}
+
+}
